27eb6d3a89a11512bc1e9a8a6ae58739d06b2682
AiKay96 <kordzadze.ana@gmail.com>
2025-07-08 16:59:06 +0400

    Reformat

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 from src.runner.db import Base
 
 from src.runner.config import settings
-from src.infra import models
+from src.infra.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
