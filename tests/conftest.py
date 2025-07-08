b8f4438762a76277442bce397976a4059bb6a71b
AiKay96 <kordzadze.ana@gmail.com>
2025-07-08 23:02:33 +0400

    Fix github actions issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,8 @@ from src.runner.config import settings
 from src.runner.db import Base
 from src.runner.setup import get_db, init_app
 
-engine = create_engine(settings.database_url + "_test", future=True)
+settings.database_url += "_test"
+engine = create_engine(settings.database_url, future=True)
 TestingSessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
