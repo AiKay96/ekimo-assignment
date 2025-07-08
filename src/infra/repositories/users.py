c28f0ce053d1e6cb3f673eac5929e44900236ac1
AiKay96 <kordzadze.ana@gmail.com>
2025-07-08 22:16:39 +0400

    Enable repository to read user

--- src/infra/repositories/users.py
+++ src/infra/repositories/users.py
@@ -1,7 +1,7 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
-from src.core.errors import ExistsError
+from src.core.errors import DoesNotExistError, ExistsError
 from src.core.users import User
 from src.infra.models.user import User as UserModel
 
@@ -19,7 +19,21 @@ class UserRepository:
         db_user = UserModel(
             username=user.username,
             hashed_password=pwd_context.hash(user.password),
-            id=user.id,
+            user_id=user.id,
         )
         self.db.add(db_user)
         self.db.commit()
+
+    def read(self, username: str) -> User:
+        user = self.db.query(UserModel).filter_by(username=username).first()
+        if not user:
+            raise DoesNotExistError
+        assert user.id is not None
+        assert user.username is not None
+        assert user.hashed_password is not None
+
+        return User(
+            id=user.id,
+            username=user.username,
+            password=user.hashed_password,
+        )
