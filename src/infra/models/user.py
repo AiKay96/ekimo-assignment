e04a0b7e743697146e209306440ff8dc8533151b
AiKay96 <kordzadze.ana@gmail.com>
2025-07-08 22:13:38 +0400

    Update user model

--- src/infra/models/user.py
+++ src/infra/models/user.py
@@ -1,14 +1,20 @@
-import uuid
+from uuid import UUID
 
-from sqlalchemy import Column, String
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.runner.db import Base
 
 
-class User(Base):  # type: ignore
+class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True)
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+
+    def __init__(self, user_id: UUID, username: str, hashed_password: str) -> None:
+        self.id = user_id
+        self.username = username
+        self.hashed_password = hashed_password
