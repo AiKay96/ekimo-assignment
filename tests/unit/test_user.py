850567a44a1a40dbf7c0c638dbb5b5cc79d4bf4c
AiKay96 <kordzadze.ana@gmail.com>
2025-07-08 22:29:43 +0400

    Add auth tests

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -2,24 +2,29 @@ from typing import Any
 
 import pytest
 
-from src.core.errors import ExistsError
-from src.infra.models.user import User as UserModel
+from src.core.errors import DoesNotExistError, ExistsError
 from src.infra.repositories.users import UserRepository
 from tests.fake import Fake
 
 
-def test_user_repository_create(db_session: Any) -> None:
+def test_should_not_read_unknown_user(db_session: Any) -> None:
+    repo = UserRepository(db_session)
+
+    with pytest.raises(DoesNotExistError):
+        repo.read(Fake().user().username)
+
+
+def test_should_persist(db_session: Any) -> None:
     repo = UserRepository(db_session)
     user = Fake().user()
 
     repo.create(user)
-    db_user = db_session.query(UserModel).filter_by(username=user.username).first()
+    db_user = repo.read(user.username)
 
-    assert db_user is not None
-    assert db_user.username == user.username
+    assert db_user == user
 
 
-def test_user_create_duplicate_raises_exists_error(db_session: Any) -> None:
+def test_should_not_create(db_session: Any) -> None:
     repo = UserRepository(db_session)
 
     user = Fake().user()
