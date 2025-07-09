ed464a7d6e1814a7dbbdfc9e5b5cdcd6266e63ec
AiKay96 <kordzadze.ana@gmail.com>
2025-07-09 19:45:57 +0400

    Ensure that user exists before testing

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,3 +1,7 @@
+import contextlib
+
+import pytest
+import requests
 from dotenv import load_dotenv
 
 from src.runner.config import settings
@@ -6,3 +10,16 @@ load_dotenv()
 BASE_URL = settings.base_url
 USERNAME = settings.username
 PASSWORD = settings.password
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ensure_user_exists() -> None:
+    user = {
+        "username": USERNAME,
+        "password": PASSWORD,
+    }
+
+    with contextlib.suppress(Exception):
+        requests.post(f"{BASE_URL}/users", json=user)
+
+    return
